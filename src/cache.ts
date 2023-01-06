632aeddd1e5620349e7cdea7f91cb9f08f0995b0
Yoann Rey <y.rey@tims.fr>
2023-01-06 16:42:39 +0100

    Adding cache emptier hook
    Signed-off-by: Yoann Rey <y.rey@tims.fr>

---
+++ src/cache.ts
@@ -0,0 +1,16 @@
+import { useEffect, useState} from 'react';
+
+export const useIsCacheEmpty = () => {
+    const [isCleared, setIsCleared] = useState(false);
+    useEffect(() => {
+        const token = localStorage.getItem('accessToken');
+        if (isCleared || !token) {
+            localStorage.clear();
+            sessionStorage.clear();
+            window.location.replace('/login');
+        }
+    }, [isCleared]);
+    return () => {
+        setIsCleared(true);
+    };
+};
