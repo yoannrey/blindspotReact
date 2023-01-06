ac712c9b15100a3f70a3d9ffa344e8126d06cc24
Yoann Rey <y.rey@tims.fr>
2023-01-06 16:42:59 +0100

    Create cache emptier hook for BlindSpot
    Signed-off-by: Yoann Rey <y.rey@tims.fr>

--- src/cache.ts
+++ src/cache.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 
 export const useIsCacheEmpty = () => {
     const [isCleared, setIsCleared] = useState(false);
